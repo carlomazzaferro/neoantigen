67159a308cf93231520bb749c4d8dd74b40f1ce6
Mazzafish <carlo.mazzaferro@gmail.com>
2016-08-26 12:37:25 -0700

    Added downstream analysis module and tests

--- Documents/CCBB/VAPr/build/lib/variantannotation/mongo_DB_export.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-
-def export(list_docs, collection_name, db_name):
-
-    client = MongoClient()
-    db = getattr(client, db_name)
-    collection = getattr(db, collection_name)
-    collection.insert_many(list_docs)
