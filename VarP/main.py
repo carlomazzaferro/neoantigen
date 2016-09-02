fa197449a3073a85d935c47d44fe8f17f1213338
Mazzafish <carlo.mazzaferro@gmail.com>
2016-09-02 11:11:04 -0700

    Downstream analysis class

--- VarP/main.py
+++ VarP/main.py
@@ -1,4 +1,4 @@
-
+"""
 import sys
 sys.path.append('/Users/carlomazzaferro/Documents/CCBB/neoantigen/VarP-master/')
 
@@ -10,7 +10,7 @@ reader_names = ['Tumor_RNA_Reader', 'Tumor_Targeted_Reader',
            'Somatic_Mutect_Reader']
 
 
-path_to_files = '/Volumes/Seagate Backup Plus Drive 1/vcf_files/varcode_to_test/'
+#path_to_files = '/Volumes/Seagate Backup Plus Drive 1/vcf_files/varcode_to_test/'
 
 
 myhandler = utils.HandleReaders(reader_names)
@@ -28,4 +28,29 @@ dataframe = myhandler.return_dataframe(protein_list, list_coding_effects)
 
 #Fasta file
 myhandler.generate_fasta_file(dataframe, path_to_files+'peps.txt')
+
+"""
+import sys
+sys.path.append('/Users/carlomazzaferro/Documents/CCBB/neoantigen/VarP-master/')
+import VarP
+from VarP import downstream_analysis
+import pandas
+from pymongo import MongoClient
+pvac_seq_out_file = '/Volumes/Seagate Backup Plus Drive/vcf_files/not_annotated/OUT_PVAQ_SEQ/MutecT_filtered.tsv'
+
+#This one contains gene expression data.
+gene_expression_file = '/Volumes/Seagate Backup Plus Drive/bam_files_extras/TCGA_normalized_deseq_RNAseq_counts.csv'
+
+df = pandas.read_csv(pvac_seq_out_file, sep='\t')
+gene_expression_file = pandas.read_csv(gene_expression_file)
+
+db_name = 'Variant_Prioritization_Workflow'
+collection_name = 'Somatic_Filtered_Variants'
+
+enricher = downstream_analysis.DataEnrichment(self.collection_name, self.db_name,
+                                                           self.test_pvac_file, self.test_gene_data_file)
+
+to_query = downstream_analysis.retrieve_chr_start_end(df)
+mylist = enricher.retrieve_multiple(to_query)
+
 
