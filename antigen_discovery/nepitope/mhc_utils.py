91103301d2a32274d4efd03f560963c82003a016
Mazzafish <carlo.mazzaferro@gmail.com>
2016-11-16 13:29:45 -0800

    Eliminated unused constructors

--- antigen_discovery/nepitope/mhc_utils.py
+++ antigen_discovery/nepitope/mhc_utils.py
@@ -1,6 +1,7 @@
 import pandas
 import os
 import glob
+from nepitope import pep_utils
 
 
 class FileConsolidation(object):
@@ -8,142 +9,56 @@ class FileConsolidation(object):
     Class to ease up the analysis of multiple files/proteins. It will take as inputs a file containing the results
     from netMHCcons and will provide methods to output the data in nicely formatted pandas dataframes that contain
     a variety of accessory information regarding the proteins in question.
-    4 different constructors provided, each one well suited for a specific input.
     """
     stable_cols = ['Pos', 'Peptide', 'ID']  #Columns that are always present
 
-    @classmethod
-    def load_full_file(cls, file_name):
-        """
-        For the situation in which a file containing all possible predictions (combinations of alleles and nmers)
-        for a single protein.
-        :param file_name: ex: file output from netMHCcons for 1 protein
-        :return:class with attributes: protein list (should be a list with 1 object), allele list
-        """
-        obj = cls()
-        obj.files = [file_name]
-        obj.allele_list = obj.get_allele_list(obj.files)
-        obj.protein_list = obj.get_prot_list(obj.files)
-
-        return obj
-
-    @classmethod
-    def load_full_multiprot_file(cls, file_name):
-        """
-        For the situation in which a file containing all possible predictions (combinations of alleles and nmers)
-        for multiple proteins.
-        :param file_name: file output from netMHCcons for more than 1 protein
-        :return:  protein list, allele list
-        """
-        obj = cls()
-        obj.files = [file_name]
-        obj.allele_list = obj.get_allele_list(obj.files)
-        obj.protein_list = obj.get_prot_list(obj.files)
-
-        return obj
-
-    @classmethod
-    def load_batches(cls, filepath, file_names):
+    def __init__(self, filepath, fasta_file, from_alignmet=False):
         """
         When netMHC or any other prediction method is run locally, then it might be the case that you'll have multiple
         files, each containing prediction for different alleles/n-mers. This method takes care of loading them all
         and consolidating them for later processing.
         :param filepath: path to files
-        :param file_names: ame of files
+        :param file_names: name of files
         :return:
         """
-        obj = cls()
-        obj.filepath = filepath
-        files = []
-        for i in file_names:
-            files.append(filepath + i)
-        obj.files = files
-        alleles = []
-        for i in file_names:
-            alleles.append(os.path.splitext(i)[0].split('_')[-1])
-        obj.allele_list = alleles
+        self.fasta = fasta_file
+        self.filepath = filepath
+        self.files = glob.glob(self.filepath + '*.xls')
+        self.allele_list = self._get_allele_list_from_file_names()
+        self.protein_list = self._get_prot_list(self.files)
 
-        return obj
-
-    @classmethod
-    def load_batch(cls, filepath, file_pattern):
-        """
-        Same as above but it will load all files give a name pattern, for instance:
-        load_batch('/data/predictions', 'netMHC_QQZWC_protein_')
-        :param filepath:
-        :param file_pattern:
-        :return:
-        """
-        obj = cls()
-
-        obj.filepath = filepath
-        obj.file_pattern = file_pattern
-        os.chdir(obj.filepath)
-        obj.files = glob.glob(filepath + file_pattern)
-        obj.allele_list = obj.get_allele_list(obj.files)
-
-        return obj
-
-    def return_df_list(self):
+    def return_df_list(self):    #USE
         """
         Returning a list of dataframes from class.files attribute
-        :return: a dataframe if only one file is provided, or a list of dataframes if multiple files are provided
+        :return: list of dataframes if multiple files are provided, one per each file.
         """
         list_dfs = []
-        if len(self.files) > 1:
-            for idx, files in enumerate(self.files):
 
-                df = pandas.read_csv(files, sep='\t', skiprows=1)
-                if 'split' in files.split('/')[-1]:
+        for idx, files in enumerate(self.files):
 
-                    processed = self.concat_sliced(df)
-                    list_dfs.append(processed)
-
-                else:
-                    df = df[['Pos', 'Peptide', 'nM', 'Rank', 'ID']]
-                    df["Allele"] = self.allele_list[idx]
-                    df = self.aggregate_info(df)
-                    list_dfs.append(df)
-
-            return list_dfs
-        else:
-            df = pandas.read_csv(self.files[0], sep='\t', skiprows=1)
-            processed = self.concat_sliced(df)
-            return processed
-
-    def concat_sliced(self, df):
-        """
-        Concatenate dataframes in list after slicing them: a netMHC prediction comes in wide format, having 3 columns
-        for each allele. Here we slice them and concatenate them in order to get a pandas dataframe in long format
-
-        :param df: wide dataframe from netMHC
-        :return: long dataframe
-        """
-
-        sliced_cols = self.slice_over_df(df)
-        list_dfs = []
-
-        for i in sliced_cols:
-            list_dfs.append(self.rename_cols(df[i]))
-
-        major = self.return_concat(self.add_allele_name(list_dfs, self.allele_list))
-
-        return major
-
-    def list_df_by_prot(self, df):
-
-        """
-        Slice long dataframe per protein and place them in a list
-        :param df: long dataframe
-        :return: list of dataframes
-        """
-        list_dfs = []
-        for i in self.protein_list:
-            df_of_prot = df.loc[df['ID'] == i]
-            df_of_prot = self.aggregate_info(df_of_prot)
-            list_dfs.append(df_of_prot)
+            df = pandas.read_csv(files, sep='\t', skiprows=1)
+            df = df[['Pos', 'Peptide', 'nM', 'Rank', 'ID']]
+            df["Allele"] = self.allele_list[idx]
+            df = self.aggregate_info(df)
+            list_dfs.append(df)
 
         return list_dfs
+
+    def optimized_list_df_by_prot(self, list_df):     #USE
+
+        concatd = pandas.concat(list_df)
+        prot_list = list(concatd.ID.unique())
+        concatd = self.aggregate_info(concatd)  ##affinity and len data
+
+        df_list_by_protein = []                 #slice by protein
+        for i in prot_list:
+            df_list_by_protein.append(concatd[concatd['ID'] == i])
+
+        orig_name_prot_list = []   #rename protein names accoridng to original fasta file
+        for i in df_list_by_protein:
+            orig_name_prot_list.append(self.replace_names_in_df(i))
+
+        return orig_name_prot_list
 
     @staticmethod
     def replace_X_with_underscore(df):
@@ -156,17 +71,7 @@ class FileConsolidation(object):
         return df
 
     @staticmethod
-    def concat_batches(list_dfs):
-        """
-        Concat pandas in a list
-        :param list_dfs: list of dfs
-        :return: concat'd df
-        """
-        conc1 = pandas.concat(list_dfs)
-        return conc1
-
-    @staticmethod
-    def label_affinity(row):
+    def label_affinity(row):      #USE
         """
         Function ot be applied to a dataframe to introduce a column with a label for the binding affinity
         :param row: row of df
@@ -181,7 +86,7 @@ class FileConsolidation(object):
         if row['nM'] > 5000.0:
             return 'No'
 
-    def aggregate_info(self, df1):
+    def aggregate_info(self, df1):      #USE
         """
         Add extra data to dataframe: affinity level label, and n-mer
         :param df1: df
@@ -214,37 +119,6 @@ class FileConsolidation(object):
         return unique_alleles
 
     @staticmethod
-    def get_prot_list(files):
-        """
-        Retrieve list of proteins from netMHC file
-        :param files: netMHC predictions
-        :return: list of proteins
-        """
-        unique_prots = []
-
-        for i in files:
-            df1 = pandas.read_csv(i, sep='\t', skiprows=1)
-            prot_IDs = list(df1['ID'].unique())
-            unique_prots.append(prot_IDs)
-
-        unique_prots = [item for sublist in unique_prots for item in sublist]
-        return unique_prots
-
-    @staticmethod
-    def slice_over_df(df):
-
-        all_cols = list(df.columns)
-        list_cols = []
-
-        for i in range(3, len(all_cols), 3):
-
-            sliced = all_cols[i:(i + 3)]
-            sliced.extend(FileConsolidation.stable_cols)
-            list_cols.append(sliced)
-
-        return list_cols
-
-    @staticmethod
     def rename_cols(df):
 
         col_names = list(df.columns)
@@ -269,9 +143,55 @@ class FileConsolidation(object):
 
         return list_dfs
 
+    def get_name_mapping(self):   #USE
+
+        orig_names = pep_utils.create_separate_lists(self.fasta)[0]
+        orig_names = [orig_name.replace('.', '_').strip('>') for orig_name in orig_names]
+        abbr_names = self.protein_list
+        name_mapping = {}
+
+        for abbr_name in abbr_names:
+            for orig_name in orig_names:
+                if orig_name.startswith(abbr_name):
+                    name_mapping[abbr_name] = orig_name
+
+        return name_mapping
+
+    def replace_names_in_df(self, df):  #USE
+
+        name_mapping = self.get_name_mapping()
+
+        for key in name_mapping.keys():
+            if df.ID.unique()[0] == key:
+                df.ID = df.ID.str.replace(key, name_mapping[key])
+        return df
+
+    def _get_file_names(self):  #USE
+        files = []
+        for i in self.file_names:
+            files.append(self.filepath + i)
+        return files
+
+    def _get_allele_list_from_file_names(self):  #USE
+
+        alleles = []
+        for file in self.files:
+            file_info = os.path.splitext(file)[0].split('_')
+
+            for inf in file_info:
+                if inf.startswith('HLA'):
+                    alleles.append(inf)
+        return alleles
+
     @staticmethod
-    def return_concat(list_dfs):
+    def _get_prot_list(files):  #USE
+        """
+        Retrieve list of proteins from netMHC file
+        :param files: netMHC predictions
+        :return: list of proteins
+        """
 
-        major_df = pandas.concat(list_dfs[:-1])
+        df1 = pandas.read_csv(files[0], sep='\t', skiprows=1)
+        prot_ids = list(df1['ID'].unique())
 
-        return major_df
+        return prot_ids
